b4313da11919f36dcf0724c849cce64e62baeb1e
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2024-01-22 22:56:17 +0100

    update modal + fix error

--- src/components/modal/modal.svelte
+++ src/components/modal/modal.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	export let open: boolean;
 	let modal: HTMLDialogElement;
-	let status: boolean = false;
+	let isModalOpen: boolean;
 	$: {
-		if ($$props.open) {
+		if (open) {
 			modal.showModal();
-			status = true;
-		} else if (status) {
+			isModalOpen = modal?.getAttribute('open') !== null;
+		}
+
+		if (isModalOpen && !open) {
 			modal.close();
-			status = false;
 		}
 	}
 
@@ -17,13 +19,10 @@
 		}
 	}
 
-	// function handleClickOutside(event: any) {
-	// 	console.log('handleClickOutside', event, modal);
-	// 	if (event.target === modal) {
-	// 		modal.close();
-	// 		status = false;
-	// 	}
-	// }
+	function handleClickOutside() {
+		modal.close();
+		open = false;
+	}
 </script>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
@@ -33,8 +32,90 @@
 		<slot />
 	</div>
 	{#if $$props.clickOutside}
-		<form method="dialog" class="modal-backdrop">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<form method="dialog" class="modal-backdrop" on:click={() => handleClickOutside()}>
 			<button>close</button>
 		</form>
 	{/if}
 </dialog>
+
+<style>
+	dialog {
+		pointer-events: none;
+		position: fixed;
+		inset: 0;
+		margin: 0;
+		display: grid;
+		height: 100%;
+		max-height: none;
+		width: 100%;
+		max-width: none;
+		justify-items: center;
+		padding: 0;
+		opacity: 0;
+		overscroll-behavior: contain;
+		z-index: 999;
+		background-color: transparent;
+		color: inherit;
+		transition-duration: 0.2s;
+		transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+		transition-property: transform, opacity, visibility;
+		overflow-y: hidden;
+	}
+
+	.modal-open,
+	.modal:target,
+	.modal-toggle:checked + .modal,
+	.modal[open] {
+		pointer-events: auto;
+		visibility: visible;
+		opacity: 1;
+	}
+
+	:where(.modal) {
+		align-items: center;
+	}
+
+	.modal-box {
+		max-height: calc(100vh - 5em);
+		grid-column-start: 1;
+		grid-row-start: 1;
+		width: 91.666667%;
+		max-width: 32rem;
+
+		border-bottom-right-radius: var(--rounded-box, 1rem);
+		border-bottom-left-radius: var(--rounded-box, 1rem);
+		border-top-left-radius: var(--rounded-box, 1rem);
+		border-top-right-radius: var(--rounded-box, 1rem);
+		background-color: white;
+		padding: 1.5rem;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+		transition-duration: 0.2s;
+		box-shadow: #00000040 0 25px 50px -12px;
+		overflow-y: auto;
+		overscroll-behavior: contain;
+	}
+
+	.modal-backdrop {
+		z-index: -1;
+		grid-column-start: 1;
+		grid-row-start: 1;
+		display: grid;
+		align-self: stretch;
+		justify-self: stretch;
+		color: transparent;
+	}
+
+	.modal-backdrop > button {
+		cursor: pointer;
+		background-color: transparent;
+		background-image: none;
+	}
+
+	.modal:not(dialog:not(.modal-open)),
+	.modal::backdrop {
+		background-color: #0006;
+		animation: modal-pop 0.2s ease-out;
+	}
+</style>
