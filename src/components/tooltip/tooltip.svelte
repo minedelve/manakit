5887dc916dc984e793f1585e92203e762380ff82
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2024-01-29 19:21:54 +0100

    WIP tooltip component

---
+++ src/components/tooltip/tooltip.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { classMap, styleMap } from '../../libs/helpers';
+</script>
+
+<div
+	id={$$props.id}
+	class={classMap({
+		component: 'tooltip',
+		default: $$props.class
+	})}
+	style={styleMap({
+		default: $$props.style
+	})}
+>
+	<slot />
+</div>
+
+<style>
+	.tooltip {
+		position: relative;
+		display: inline-block;
+		text-align: center;
+	}
+</style>
