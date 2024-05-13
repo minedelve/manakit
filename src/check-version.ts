39ebd6f04ce81faf586111717a7e6749074a193b
Nycolaide <laurent.grimaldi@minedelve.com>
2024-05-13 21:46:15 +0200

    #8 - modify process to call api npm version with try catch

--- src/check-version.ts
+++ src/check-version.ts
@@ -3,12 +3,26 @@ import packageVersions from 'pkg-versions';
 import pkg from '../package.json' assert { type: 'json' };
 
 export async function checkVersion() {
-	const versions = await packageVersions('manakit');
-	const format = new Set(versions);
-	const list = Array.from(format);
-	const lastestVersion = list[list.length - 1];
+	let lastestVersion;
+	try {
+		const versions = await packageVersions('manakit');
+		const format = new Set(versions);
+		const list = Array.from(format);
+		lastestVersion = list[list.length - 1];
+	} catch (error) {
+		console.log(
+			chalk.red(
+				`The manakit version check with the remote server failed, check your internet connection or your proxy configuration`
+			)
+		);
+		console.log(
+			chalk.red(
+				`(verification is not mandatory, you can desactivate it in the manakit configuration)`
+			)
+		);
+	}
 
-	if (pkg.version !== lastestVersion) {
+	if (lastestVersion && pkg.version !== lastestVersion) {
 		console.log(
 			chalk.bold(
 				`\n\nManakit update available ` +
