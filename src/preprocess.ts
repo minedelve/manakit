49b25f6b4d97533ceb15becfbd5497d890d04586
Nycolaide <laurent.grimaldi@minedelve.com>
2024-05-13 21:44:17 +0200

    #8 - add params on preprocess ManakitVite for disabled call api version and install configuration

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -2,12 +2,24 @@ import { checkInstall } from './check-install.js';
 import { checkVersion } from './check-version.js';
 import { makeManakitImporterConfig } from './importer.js';
 
-export function manakit() {
+interface ManakitVite {
+	verif?: {
+		version?: boolean | undefined;
+		install?: boolean | undefined;
+	};
+}
+
+export function manakit(params?: ManakitVite) {
+	let verifVersion = true;
+	let verifInstall = true;
+
+	if (params?.verif?.version !== undefined) verifVersion = params?.verif?.version;
+	if (params?.verif?.install !== undefined) verifInstall = params?.verif?.install;
 	return {
 		name: 'manakit-preprocess',
 		async buildStart() {
-			await checkVersion();
-			await checkInstall();
+			if (verifVersion) await checkVersion();
+			if (verifInstall) await checkInstall();
 		},
 		async configResolved() {
 			await makeManakitImporterConfig();
