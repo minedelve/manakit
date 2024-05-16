21937bee509b94d4cc3ab9c205c2d8344af56778
Nycolaide <laurent.grimaldi@minedelve.com>
2024-05-16 22:44:26 +0200

    new system sass and css for optimise CSS

--- src/utils/path-scss.ts
+++ src/utils/path-scss.ts
@@ -6,52 +6,25 @@ const nodePathModules = 'node_modules/manakit/dist/modules';
 
 // Files SCSS
 export const headSCSS = [
-	path.resolve(`${nodePath}`, 'variables.scss'),
-	path.resolve(`${nodePath}`, 'mixins.scss'),
-	path.resolve(`${nodePath}`, 'normalize.scss')
+	path.resolve(`${nodePath}`, 'initial.scss'),
+	path.resolve(`${nodePath}`, 'mixins.scss')
 ];
+
+export const variablesSCSS = [path.resolve(`${nodePath}`, 'variables.scss')];
 
 export const paletteSCSS = {
 	manakit: path.resolve(`${nodePathColors}`, 'manakit.scss'),
 	webkit: path.resolve(`${nodePathColors}`, 'webkit.scss')
 };
 
-export const contentSCSS = [
-	path.resolve(`${nodePathLibs}`, 'prose.scss'),
-	path.resolve(`${nodePathLibs}`, 'align-content.scss'),
-	path.resolve(`${nodePathLibs}`, 'align-item.scss'),
-	path.resolve(`${nodePathLibs}`, 'align-self.scss'),
-	path.resolve(`${nodePathLibs}`, 'auto-margin.scss'),
-	path.resolve(`${nodePathLibs}`, 'column.scss'),
-	path.resolve(`${nodePathLibs}`, 'direction.scss'),
-	path.resolve(`${nodePathLibs}`, 'display.scss'),
-	path.resolve(`${nodePathLibs}`, 'float.scss'),
-	path.resolve(`${nodePathLibs}`, 'gap.scss'),
-	path.resolve(`${nodePathLibs}`, 'grow-shrink.scss'),
-	path.resolve(`${nodePathLibs}`, 'grow.scss'),
-	path.resolve(`${nodePathLibs}`, 'height.scss'),
-	path.resolve(`${nodePathLibs}`, 'justify.scss'),
-	path.resolve(`${nodePathLibs}`, 'margin.scss'),
-	path.resolve(`${nodePathLibs}`, 'order.scss'),
-	path.resolve(`${nodePathLibs}`, 'padding.scss'),
-	path.resolve(`${nodePathLibs}`, 'position.scss'),
-	path.resolve(`${nodePathLibs}`, 'rounded.scss'),
-	path.resolve(`${nodePathLibs}`, 'row.scss'),
-	path.resolve(`${nodePathLibs}`, 'shadow.scss'),
-	path.resolve(`${nodePathLibs}`, 'shortland.scss'),
-	path.resolve(`${nodePathLibs}`, 'text-align.scss'),
-	path.resolve(`${nodePathLibs}`, 'text-decoration.scss'),
-	path.resolve(`${nodePathLibs}`, 'text-opacity.scss'),
-	path.resolve(`${nodePathLibs}`, 'text-size.scss'),
-	path.resolve(`${nodePathLibs}`, 'text-transform.scss'),
-	path.resolve(`${nodePathLibs}`, 'text-weight.scss'),
-	path.resolve(`${nodePathLibs}`, 'width.scss'),
-	path.resolve(`${nodePathLibs}`, 'wrap.scss'),
-	path.resolve(`${nodePathLibs}`, 'colors.scss')
+export const globalSCSS = [
+	path.resolve(`${nodePathLibs}`, 'global.scss'),
+	path.resolve(`${nodePathLibs}`, 'normalize.scss'),
+	path.resolve(`${nodePathLibs}`, 'colors.scss'),
+	path.resolve(`${nodePathLibs}`, 'prose.scss')
 ];
 
 export const modulesSCSS = [
-	path.resolve(`${nodePath}`, 'main.scss'),
 	path.resolve(`${nodePathModules}/app`, 'app.scss'),
 	path.resolve(`${nodePathModules}/spacer`, 'spacer.scss'),
 	path.resolve(`${nodePathModules}/toolbar`, 'toolbar.scss'),
